e3cd972b0539ed2b5c76550187778701ba9a0090
Richard Abbenhuis <richard.abbenhuis@tennet.eu>
2017-03-14 17:29:19 +0100

    dos2unix provisioner.sh

--- salt/bin/provisioner.sh
+++ salt/bin/provisioner.sh
@@ -5,5 +5,5 @@ if which salt-call >/dev/null; then
     salt --version
 else
     echo salt-call does not exist
-    curl -L https://bootstrap.saltstack.com -o install_salt.sh | sudo sh install_salt.sh
+    curl -L https://bootstrap.saltstack.com -o install_salt.sh | sudo bash install_salt.sh
 fi
